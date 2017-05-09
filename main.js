95d1a2264e23288ed061248e8755ebaf29b3ece3
rreimann <robert.reimann@googlemail.com>
2017-05-09 11:02:41 +0200

    add sample for pdf plugin ignoring 'plugins: false' option

--- main.js
+++ main.js
@@ -13,7 +13,13 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    webPreferences: {
+      plugins: false  // set it explicitly to false even if it should be the default
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
